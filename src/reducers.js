b905c1c32f41c8025eb73c2d72c0193cc2485928
grindos <grindosart@gmail.com>
2018-09-07 12:28:46 +0300

    Rename files with reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import streams from './containers/App/StreamsList/reducers';
+import streams from './containers/App/StreamsList/reducer';
 
 export default combineReducers({
   streams,
